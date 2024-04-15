a717f03e664a10e4c679517b77c3ad2da6d6ca97
Huynh Huu Nghia <nghiabatuoidev@gmail.com>
2024-04-15 16:22:09 +0700

    first commit

--- app/src/androidTest/java/com/uit/quanlychitieu/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/uit/quanlychitieu/ExampleInstrumentedTest.java
@@ -9,6 +9,8 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+import androidx.test.platform.app.InstrumentationRegistry;
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
